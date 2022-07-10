7943f74c950c507fad5d97cc2ccefed58f11556c
spkaluzny <spkaluzny@gmail.com>
2022-07-10 13:39:58 -0700

    Added Sfloat, Sint typedefs

--- src/seriesFuns.h
+++ src/seriesFuns.h
@@ -12,6 +12,12 @@
 #include <Rdefines.h>
 #include <Rversion.h>
 
+/* Sfloat, Sint added to splusTimeSeries_1.5.5 as they will be dropped     
+ * from R soon.
+ */
+typedef double Sfloat;
+typedef int Sint;
+
 void series_lag(double *y, Sint *nrow, Sint *ncol, Sint *p, 
 		Sint *np, double *ylag);
 
